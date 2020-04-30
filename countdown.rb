c62ee1327dd2c0fd88c56088380529f392f14267
Nick Lanese <nlanese21@gmail.com>
2020-04-30 18:59:50 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,12 @@ def countdown(currentVal)
   end
   return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(currentVal)
+  while currentVal > 0
+    puts "#{currentVal} SECOND(S)!"
+    currentVal -= 1
+    sleep(1)
+  end
+  return "HAPPY NEW YEAR!"
+end
